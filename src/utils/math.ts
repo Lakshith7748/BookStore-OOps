305ccc21ff1fa83261f76d4a15ef99a7f51d9110
Lakshith <lakshith.achuta@gmail.com>
2025-11-17 10:00:00 +0530

    Add modulo function to math utils

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -2,3 +2,4 @@ export const add = (a: number, b: number) => a + b;
 export const subtract = (a: number, b: number) => a - b;
 export const multiply = (a: number, b: number) => a * b;
 export const divide = (a: number, b: number) => a / b;
+export const modulo = (a: number, b: number) => a % b;
