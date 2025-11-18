e7de6e57684058883fc6924b780e5e2b2bd5d8de
Lakshith <lakshith.achuta@gmail.com>
2025-11-18 10:00:00 +0530

    Add root function to math utils

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -4,3 +4,4 @@ export const multiply = (a: number, b: number) => a * b;
 export const divide = (a: number, b: number) => a / b;
 export const modulo = (a: number, b: number) => a % b;
 export const power = (a: number, b: number) => Math.pow(a, b);
+export const root = (a: number) => Math.sqrt(a);
