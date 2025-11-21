298b32ee893707e69c30963045db8f0da650909e
Lakshith <lakshith.achuta@gmail.com>
2025-11-21 16:00:00 +0530

    Add reverseStr function

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,2 +1,3 @@
 export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 export const uncapitalize = (s: string) => s.charAt(0).toLowerCase() + s.slice(1);
+export const reverseStr = (s: string) => s.split('').reverse().join('');
