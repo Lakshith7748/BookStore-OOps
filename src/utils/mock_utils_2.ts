98ce541a82a1f750fe9dccfc25b3a5c33c3fcb38
Lakshith <lakshith.achuta@gmail.com>
2025-12-05 16:00:00 +0530

    Implement additional robust mock utility 25 for testing

--- src/utils/mock_utils_2.ts
+++ src/utils/mock_utils_2.ts
@@ -3,3 +3,4 @@ export const mockUtil_21 = () => 'Mock util version 21';
 export const mockUtil_22 = () => 'Mock util version 22';
 export const mockUtil_23 = () => 'Mock util version 23';
 export const mockUtil_24 = () => 'Mock util version 24';
+export const mockUtil_25 = () => 'Mock util version 25';
