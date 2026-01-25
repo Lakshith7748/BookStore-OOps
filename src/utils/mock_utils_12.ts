8e284f26b2edce1005600128d86325dcdac307ab
Lakshith <lakshith.achuta@gmail.com>
2026-01-25 10:00:00 +0530

    Implement additional robust mock utility 126 for testing

--- src/utils/mock_utils_12.ts
+++ src/utils/mock_utils_12.ts
@@ -4,3 +4,4 @@ export const mockUtil_122 = () => 'Mock util version 122';
 export const mockUtil_123 = () => 'Mock util version 123';
 export const mockUtil_124 = () => 'Mock util version 124';
 export const mockUtil_125 = () => 'Mock util version 125';
+export const mockUtil_126 = () => 'Mock util version 126';
