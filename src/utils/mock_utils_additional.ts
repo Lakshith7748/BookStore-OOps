eacfb03164fc171ef2d4462808baf9953d034855
Lakshith <lakshith.achuta@gmail.com>
2026-02-16 10:00:00 +0530

    Add mockAdditional_2 extra utility function for robustness

--- src/utils/mock_utils_additional.ts
+++ src/utils/mock_utils_additional.ts
@@ -1,2 +1,3 @@
 export const mockAdditional_0 = () => 'Mock extra util version 0';
 export const mockAdditional_1 = () => 'Mock extra util version 1';
+export const mockAdditional_2 = () => 'Mock extra util version 2';
